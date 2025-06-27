6bd734810796663a861528127ed5674e93257582
wlqgkrry <wlqgkrry@gmail.com>
2025-06-28 06:19:59 +0900

    [FIX] 투표 결과 조회(results)와 투표 항목 조회(items)의 response json을 PARTLEADER: 2차원 배열, DEMODAY: 1차원 배열로 변환하는 로직 구현

--- src/main/java/vote/dream/server/domain/vote/controller/VoteController.java
+++ src/main/java/vote/dream/server/domain/vote/controller/VoteController.java
@@ -22,17 +22,38 @@ public class VoteController {
     @Autowired
     private VoteService voteService;
 
-    // 1. 투표 항목 목록 조회 (비로그인 허용)
+//    // 1. 투표 항목 목록 조회 (비로그인 허용) // 가장 처음 코드
+//    @GetMapping("/{voteType}/items")
+//    public ApiResponse<List<VoteItemDto>> getVoteItems(@PathVariable VoteType voteType) {
+//        return ApiResponse.onSuccess(voteService.getVoteItems(voteType));
+//    }
+
+//    @GetMapping("/{voteType}/items")
+//    public ApiResponse<List<List<VoteItemDto>>> getVoteItems(@PathVariable VoteType voteType) {
+//        return ApiResponse.onSuccess(voteService.getVoteItemsGroupedByTeam(voteType));
+//    }
+
+    // 1. 투표 항목 목록 조회 -> PARTLEADER만 2차원 배열, DEMODAY는 1차원 배열로 변환(막판)
     @GetMapping("/{voteType}/items")
-    public ApiResponse<List<VoteItemDto>> getVoteItems(@PathVariable VoteType voteType) {
+    public ApiResponse<?> getVoteItems(@PathVariable VoteType voteType) {
         return ApiResponse.onSuccess(voteService.getVoteItems(voteType));
     }
 
-    // 2. 투표 결과 조회 (비로그인 허용)
+
+//    // 2. 투표 결과 조회 (비로그인 허용) -> 가장 처음 코드
+//    @GetMapping("/{voteType}/results")
+//    public ApiResponse<List<VoteResponseDto>> getVoteResults(@PathVariable VoteType voteType) {
+//        return ApiResponse.onSuccess(voteService.getVoteResults(voteType));
+//    }
+
+    // 2. 투표 결과 조회 -> PARTLEADER만 2차원 배열, DEMODAY는 1차원 배열로 변환(막판)
+    // 반환 타입을 와일드카드로 두면 두 경우 모두 커버
     @GetMapping("/{voteType}/results")
-    public ApiResponse<List<VoteResponseDto>> getVoteResults(@PathVariable VoteType voteType) {
+    public ApiResponse<?> getVoteResults(@PathVariable VoteType voteType) {
         return ApiResponse.onSuccess(voteService.getVoteResults(voteType));
     }
+
+
 
     // 3. 내 투표 여부 확인 (로그인 필요)
     @GetMapping("/{voteType}/status")
